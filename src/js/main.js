0da59c431f071d334df471c4e5783c98e2c46b23
DolanL <dylan_leijten@hotmail.com>
2018-03-07 19:20:31 +0100

    Fixed main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 //        first_name+last_name
-$( document ).ready(function() {
-    function myfunction() {
-        var first_name = document.getElementById("first_name").value;
-        var last_name = document.getElementById("last_name").value;
-        document.getElementById("full_name").innerHTML = first_name + " " + last_name;
-    }
-});
+function myfunction() {
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
+    document.getElementById("full_name").innerHTML = first_name + " " + last_name;
+}
